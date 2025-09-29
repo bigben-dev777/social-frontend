0c9c4a1ae5b553f889d51dbbc52d4e67762fc084
bigben-dev777 <albertojohn20041103@gmail.com>
2025-09-29 15:03:47 -0400

    ✔ delete unnessary code

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,38 +1,3 @@
-interface AuthHeader {
-  Authorization: string;
-}
-
-export function getAuthorizedTokenHeader(): Partial<AuthHeader> {
-  const token = localStorage.getItem('token');
-  const header = token ? { Authorization: `Bearer ${token}` } : {};
-
-  return header;
-}
-
-export function setTokenAtLocal(token: string): void {
-  localStorage.setItem('token', token);
-}
-
-// function stringToColor(string: string) {
-//   let hash = 0;
-//   let i;
-
-//   /* eslint-disable no-bitwise */
-//   for (i = 0; i < string.length; i += 1) {
-//     hash = string.charCodeAt(i) + ((hash << 5) - hash);
-//   }
-
-//   let color = '#';
-
-//   for (i = 0; i < 3; i += 1) {
-//     const value = (hash >> (i * 8)) & 0xff;
-//     color += `00${value.toString(16)}`.slice(-2);
-//   }
-//   /* eslint-enable no-bitwise */
-
-//   return color;
-// }
-
 export function stringAvatar(name: string) {
   return {
     sx: {
